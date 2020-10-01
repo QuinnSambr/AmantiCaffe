a401c5f6cf14a77d83279af96b31086b7c4a3f00
QuinnyBoy-TT <59735361+QuinnyBoy-TT@users.noreply.github.com>
2020-10-01 12:14:18 -0400

    commit

--- armanteSite/settings.py
+++ armanteSite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'posts/static'
+STATIC_ROOT = '/posts/static'
 
 
 MEDIA_URL='/media/'

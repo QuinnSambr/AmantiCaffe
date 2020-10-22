1fbe5673547ee99e73709faa7a4ed61e8ec81f57
QuinnyBoy-TT <59735361+QuinnyBoy-TT@users.noreply.github.com>
2020-10-22 01:09:14 -0400

    commit

--- armanteSite/settings.py
+++ armanteSite/settings.py
@@ -126,11 +126,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
